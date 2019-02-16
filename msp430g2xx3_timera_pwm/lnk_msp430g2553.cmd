5cab0bcca433be7da468ab9fecbcb3346006ea27
hijazifm <fred.hijazi@leo-technologies.com>
2019-02-16 03:37:57 +0100

    test

--- msp430g2xx3_timera_pwm/lnk_msp430g2553.cmd
+++ msp430g2xx3_timera_pwm/lnk_msp430g2553.cmd
@@ -1,6 +1,7 @@
 /* ============================================================================ */
 /* Copyright (c) 2015, Texas Instruments Incorporated                           */
 /*  All rights reserved.                                                        */
+
 /*                                                                              */
 /*  Redistribution and use in source and binary forms, with or without          */
 /*  modification, are permitted provided that the following conditions          */
